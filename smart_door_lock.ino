bdd71cd8c0cdd420f33e0114db0a5fc5d85e4460
amrsallam10 <amrmohsallam@aucegypt.edu>
2024-05-08 16:25:12 +0300

    implemented timout, lockout and interfacing with the owner through bluetooth(state, lock, unlock, set passcode)

--- smart_door_lock.ino
+++ smart_door_lock.ino
@@ -8,13 +8,18 @@
 // #define SERVO_UNLOCK_POS 90
 // Servo lockServo;
 
-const int pinR = 13;
-const int pinG = 12;
-const int pinB = 11;
-const int pinBuzz = 22;
-const byte Serial1RxPin = 19;
+#define PINR 13
+#define PING 12
+#define PINB 11
+#define PINBUZZ 22
+#define SERIAL1_RX_PIN 19
+#define MAX_CODE_LENGTH 4
+#define MAX_ATTEMPTS 3
+#define TIME_OUT 5000 // 5 seconds
+#define LOCK_OUT_TIME 20000 // 20 seconds
+#define PASSCODE "1919"
 
-const int startupMelody[] = {262, 330, 392, 523}; // C4, E4, G4, C5
+const int startupMelody[] = {262, 330, 392, 523};
 const int openCloseMelody = 2000;  
 const int alartMelody = 1000;  
 const int startupNoteDuration = 200; 
@@ -23,6 +28,8 @@ const int alarmNoteDuration = 1000;
 
 volatile String command = "";
 volatile bool userMode = true;
+
+int attempts = 0;
 
 /* Keypad setup */
 const byte KEYPAD_ROWS = 4;
@@ -38,30 +45,30 @@ char keys[KEYPAD_ROWS][KEYPAD_COLS] = {
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS);
 
-/* SafeState stores the secret code in EEPROM */
 DoorState doorState;
 
-void showStartupMessage() {
-  for (int i = 0; i < 4; i++) {
-    tone(pinBuzz, startupMelody[i], startupNoteDuration);
+void playStartupTone() {
+  Serial.println("Starting up...");
+  for (int i = 0; i < MAX_CODE_LENGTH; i++) {
+    tone(PINBUZZ, startupMelody[i], startupNoteDuration);
     delay(startupNoteDuration);
-    noTone(pinBuzz);
+    noTone(PINBUZZ);
     delay(50); 
   }
 }
 
-void playOpenCloseMelody() {
-    tone(pinBuzz, openCloseMelody, openCloseNoteDuration);
+void playLockUnlockTone() {
+    tone(PINBUZZ, openCloseMelody, openCloseNoteDuration);
     delay(openCloseNoteDuration);
-    noTone(pinBuzz);
+    noTone(PINBUZZ);
     delay(50);  
 }
 
 void playAlartMelody() {
   for (int i = 0; i < 3; i++) {
-    tone(pinBuzz, alartMelody, alarmNoteDuration);
+    tone(PINBUZZ, alartMelody, alarmNoteDuration);
     delay(alarmNoteDuration);
-    noTone(pinBuzz);
+    noTone(PINBUZZ);
     delay(200); // Pause between notes
   }
   delay(1000); // Pause before repeating the alarm (adjust as needed)
@@ -70,61 +77,75 @@ void playAlartMelody() {
 void lock() {
   // lockServo.write(SERVO_LOCK_POS);
   doorState.lock();
+  playLockUnlockTone();
 }
 
 void unlock() {
   // lockServo.write(SERVO_UNLOCK_POS);
+  playLockUnlockTone();
 }
 
-void showUnlockedStat() {
-  digitalWrite(pinR, HIGH);
-  digitalWrite(pinG, LOW);
-  digitalWrite(pinB, HIGH);
+void alart(){
+  displayAlartStateOnLED();
+  playAlartMelody();
+}
+
+void displayUnlockedStateOnLED() {
+  digitalWrite(PINR, HIGH);
+  digitalWrite(PING, LOW);
+  digitalWrite(PINB, HIGH);
   Serial.println("Unlocked");
 }
 
-void showLockedStat() {
-  digitalWrite(pinR, HIGH);
-  digitalWrite(pinG, HIGH);
-  digitalWrite(pinB, LOW);
+void displayLockedStateOnLED() {
+  digitalWrite(PINR, HIGH);
+  digitalWrite(PING, HIGH);
+  digitalWrite(PINB, LOW);
   Serial.println("Locked");
 }
 
-void showAlartMsg() {
-  digitalWrite(pinR, LOW);
-  digitalWrite(pinG, HIGH);
-  digitalWrite(pinB, HIGH);
+void displayAlartStateOnLED() {
+  digitalWrite(PINR, LOW);
+  digitalWrite(PING, HIGH);
+  digitalWrite(PINB, HIGH);
   Serial.println("Access Denied");
 }
 
 String inputSecretCode() {
   String result = "";
+  unsigned long lastKeyPressTime = 0;
   while (userMode && result.length() < 4) {
     char key = keypad.getKey();
     if (key != NO_KEY) {
+      if (result.length() == 0) {
+        lastKeyPressTime = millis();
+      }
       result += key;
       Serial.println(result);
+      lastKeyPressTime = millis();
+    }
+
+    if (lastKeyPressTime != 0 && millis() - lastKeyPressTime > TIME_OUT) {
+      Serial.println("Time out");
+      return "";
     }
   }
   return result;
 }
 
 void doorUnlockedLogic() {
-  showUnlockedStat();
+  displayUnlockedStateOnLED();
   auto key = keypad.getKey();
   while (key != 'C') {
     if (!userMode)
       return;
     key = keypad.getKey();
   }
-
-  doorState.lock();
   lock();
-  playOpenCloseMelody();
 }
 
 void doorLockedLogic() {
-  showLockedStat();
+  displayLockedStateOnLED();
 
   String userCode = inputSecretCode();
   if (userMode){
@@ -132,10 +153,16 @@ void doorLockedLogic() {
     delay(200);
     if (unlockedSuccessfully) {
       unlock();
-      playOpenCloseMelody();
     } else {
-      showAlartMsg();
-      playAlartMelody();
+      alart();
+      attempts++;
+      Serial.println("Attempts: " + String(attempts));
+      if (attempts >= MAX_ATTEMPTS) {
+        Serial.println("Locking out for 20 seconds");
+        delay(LOCK_OUT_TIME);
+        attempts = 0;
+        Serial.println("Locking out time is over");
+      }
     }
   }
 }
@@ -147,14 +174,16 @@ void parseCommand(){
     } else {
       Serial1.println("Unlocked");
     }
-  } else if (command.startsWith("close")) {
-    doorState.lock();
+  } else if (command.startsWith("lock")) {
     lock();
-    playOpenCloseMelody();
-  } else if (command.startsWith("open")) {
-    doorState.unlock("1919");
+  } else if (command.startsWith("unlock")) {
+    doorState.unlock(PASSCODE);
     unlock();
-    playOpenCloseMelody();
+  } else if (command.startsWith("code")) {
+    String newCode = command.substring(4, 8);
+    doorState.setCode(newCode);
+    Serial1.println("Code set to: " + newCode);
+    Serial.println("Setting new passcode");
   }
   command = "";
 }
@@ -165,7 +194,9 @@ void readFromSerial1(){
     command += receivedChar;
     Serial.print(receivedChar);
 
-    if (command.startsWith("state") || command.startsWith("close") || command.startsWith("open")) {
+    if (command.startsWith("state") || command.startsWith("lock") || command.startsWith("unlock") || 
+                                            (command.startsWith("code") && command.length() == 8)) {
+      Serial.print('\n');
       userMode = false;
     } else if (receivedChar == '\n') {
         command = ""; 
@@ -173,26 +204,29 @@ void readFromSerial1(){
   }
 }
 
+void timerIsr() {
+  userMode = true;
+}
+
 void setup() {
 
-  pinMode(pinR, OUTPUT);
-  pinMode(pinG, OUTPUT);
-  pinMode(pinB, OUTPUT);
-  pinMode(pinBuzz, OUTPUT);
-  pinMode(Serial1RxPin, INPUT_PULLUP); 
+  pinMode(PINR, OUTPUT);
+  pinMode(PING, OUTPUT);
+  pinMode(PINB, OUTPUT);
+  pinMode(PINBUZZ, OUTPUT);
+  pinMode(SERIAL1_RX_PIN, INPUT_PULLUP); 
 
-  digitalWrite(pinR, HIGH);
-  digitalWrite(pinG, HIGH);
-  digitalWrite(pinB, HIGH);
+  digitalWrite(PINR, HIGH);
+  digitalWrite(PING, HIGH);
+  digitalWrite(PINB, HIGH);
 
   Serial.begin(9600); // Initialize Serial Monitor
   Serial1.begin(9600);  // Initialize Serial communication with HM-10
-  attachInterrupt(digitalPinToInterrupt(Serial1RxPin), readFromSerial1, FALLING); // Interrupt for Rx1 (from HM-10)
+  attachInterrupt(digitalPinToInterrupt(SERIAL1_RX_PIN), readFromSerial1, FALLING); // Interrupt for Rx1 (from HM-10)
 
-  String code = "1919";
-  doorState.setCode(code);
+  doorState.setCode(PASSCODE);
   lock();
-  showStartupMessage();
+  playStartupTone();
 }
 
 void loop() {
