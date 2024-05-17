0e9350f3ca3f347e562e8ee0e5f4df98cbdbacc5
amrsallam10 <amrmohsallam@aucegypt.edu>
2024-05-17 23:12:54 +0300

    initial implementation of UART using register manipulation

--- smart_door_lock.ino
+++ smart_door_lock.ino
@@ -1,3 +1,36 @@
+// ---------------------------------------- CONNECTIONS ----------------------------------------
+
+// KEYPAD
+/*
+ROWS (UPPER PINS): 4, 3, 2, 1, GND, VCC
+PINS:              9, 8, 7, 6, GND, 3.3V
+
+COLS (DOWN PINS) : 4, 3, 2, 1, GND, VCC
+PINS:              5, 4, 3, 2, GND, 3.3V
+*/
+
+// BUZZER
+/*
++ to 41
+- to GND
+*/
+
+// LED (R(common Anode)GB)
+/*
+R to 37
+Anode to 5V
+G to 35
+B to 33
+*/
+
+// HM-10
+/*
+VCC to 5V
+GND to GND
+TX to 19 (Rx1 - interrupt pin)
+RX to 18 (Tx1)
+*/
+
 #include <Keypad.h>
 // #include <Servo.h>
 #include "DoorState.h"
@@ -8,10 +41,26 @@
 // #define SERVO_UNLOCK_POS 90
 // Servo lockServo;
 
-#define PINR 13
-#define PING 12
-#define PINB 11
-#define PINBUZZ 22
+// MEGA Interrupts pins: 2, 3, 18, 19, 20, 21
+// UNO Interrupts pins: 2, 3
+// NANO Interrupts pins: 2, 3
+
+// ---[ Arduino Pin Manipulation Macros ]---
+#define SET_PIN_MODE(port, pin, mode) \
+  if (mode == INPUT) { \
+    DDR ## port &= ~(1 << pin); \
+  } else if (mode == OUTPUT) { \
+    DDR ## port |= (1 << pin); \
+  }
+#define SET_PIN_HIGH(port, pin) (PORT ## port |= (1 << pin))
+#define SET_PIN_LOW(port, pin) ((PORT ## port) &= ~(1 << (pin)))
+//-----------------------------------------
+
+
+#define PINR 37
+#define PING 35
+#define PINB 33
+#define PINBUZZ 41
 #define SERIAL1_RX_PIN 19
 #define MAX_CODE_LENGTH 4
 #define MAX_ATTEMPTS 3
@@ -20,10 +69,10 @@
 #define PASSCODE "1919"
 
 const int startupMelody[] = {262, 330, 392, 523};
-const int openCloseMelody = 2000;  
+const int lockUnlockMelody = 2000;  
 const int alartMelody = 1000;  
 const int startupNoteDuration = 200; 
-const int openCloseNoteDuration = 200;              
+const int lockUnlockNoteDuration = 200;              
 const int alarmNoteDuration = 1000;
 
 volatile String command = "";
@@ -34,6 +83,7 @@ int attempts = 0;
 /* Keypad setup */
 const byte KEYPAD_ROWS = 4;
 const byte KEYPAD_COLS = 4;
+
 byte rowPins[KEYPAD_ROWS] = {2, 3, 4, 5};
 byte colPins[KEYPAD_COLS] = {6, 7, 8, 9};
 char keys[KEYPAD_ROWS][KEYPAD_COLS] = {
@@ -55,13 +105,14 @@ void playStartupTone() {
     noTone(PINBUZZ);
     delay(50); 
   }
+  delay(1000); 
 }
 
 void playLockUnlockTone() {
-    tone(PINBUZZ, openCloseMelody, openCloseNoteDuration);
-    delay(openCloseNoteDuration);
+    tone(PINBUZZ, lockUnlockMelody, lockUnlockNoteDuration);
+    delay(lockUnlockNoteDuration);
     noTone(PINBUZZ);
-    delay(50);  
+    delay(1000); 
 }
 
 void playAlartMelody() {
@@ -69,9 +120,9 @@ void playAlartMelody() {
     tone(PINBUZZ, alartMelody, alarmNoteDuration);
     delay(alarmNoteDuration);
     noTone(PINBUZZ);
-    delay(200); // Pause between notes
+    delay(200); 
   }
-  delay(1000); // Pause before repeating the alarm (adjust as needed)
+  delay(1000); 
 }
 
 void lock() {
@@ -170,9 +221,9 @@ void doorLockedLogic() {
 void parseCommand(){
   if (command.startsWith("state")) {
     if (doorState.locked()){
-      Serial1.println("Locked");
+      sendStringSerial1("Locked");
     } else {
-      Serial1.println("Unlocked");
+      sendStringSerial1("Unlocked");
     }
   } else if (command.startsWith("lock")) {
     lock();
@@ -182,15 +233,14 @@ void parseCommand(){
   } else if (command.startsWith("code")) {
     String newCode = command.substring(4, 8);
     doorState.setCode(newCode);
-    Serial1.println("Code set to: " + newCode);
+    sendStringSerial1("Code set to: " + newCode);
     Serial.println("Setting new passcode");
-  }
+  } 
   command = "";
 }
 
-void readFromSerial1(){
-  if (Serial1.available()) {  // Check if data is available from HM-10
-    char receivedChar = Serial1.read();  // Read the incoming byte
+ISR(USART1_RX_vect) {
+    char receivedChar = UDR1;  // Read the incoming byte
     command += receivedChar;
     Serial.print(receivedChar);
 
@@ -201,11 +251,36 @@ void readFromSerial1(){
     } else if (receivedChar == '\n') {
         command = ""; 
     }
-  }
 }
 
-void timerIsr() {
-  userMode = true;
+void sendCharSerial1(char c) {
+    // Wait for empty transmit buffer
+    while (!(UCSR1A & (1 << UDRE1)));
+    // Put data into buffer, sends the data
+    UDR1 = c;
+}
+
+void sendStringSerial1(const String& str) {
+    for (int i = 0; i < str.length(); i++) {
+        sendCharSerial1(str[i]);
+    }
+}
+
+// setup uart for HM-10
+void UART1_setup(){
+  // Set baud rate
+  uint16_t baud_setting = F_CPU / 16 /9600  - 1;
+  UBRR1H = baud_setting >> 8;
+  UBRR1L = baud_setting;
+
+  // Enable receiver and transmitter
+  UCSR1B |= (1 << RXEN1) | (1 << TXEN1);
+
+  // Enable Rx interrupt
+  UCSR1B |= (1 << RXCIE1);
+
+  // Set 8-bit character sizes
+  UCSR1C |= (1 << UCSZ11) | (1 << UCSZ10);
 }
 
 void setup() {
@@ -221,8 +296,7 @@ void setup() {
   digitalWrite(PINB, HIGH);
 
   Serial.begin(9600); // Initialize Serial Monitor
-  Serial1.begin(9600);  // Initialize Serial communication with HM-10
-  attachInterrupt(digitalPinToInterrupt(SERIAL1_RX_PIN), readFromSerial1, FALLING); // Interrupt for Rx1 (from HM-10)
+  UART1_setup();
 
   doorState.setCode(PASSCODE);
   lock();
